fa58e5d6b9b3810ca9a30531d26375a8e04e2c91
Saurabh <saurabhshivam4@gmail.com>
2021-09-24 01:14:12 +0530

    Level-2 Database Encryption and using environment variables to keep secrets safe (using .env file)

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const encrypt = require("mongoose-encryption");
 
 const homeStartingContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,comes from a line in section 1.10.32.";
 const aboutPageContent = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of. The Extremes of Good and Evil) by Cicero";
 const contactPageContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,comes from a line in section 1.10.32.";
 
 const app = express();
+
+// console.log(process.env.API_KEY);
 
 app.set('view engine', 'ejs');
 
@@ -48,11 +52,20 @@ const Join = mongoose.model("Join", joinSchema);
 
 
 // CREATING MONGOOSE SCHEMA FOR AUTHENTICATION
-const userSchema = {
+// This is an object created from mongoose schema class
+const userSchema = new mongoose.Schema({
   Username: String,
   Email: String,
   Password: String
-}
+});
+
+// Added encryption package to userSchema, defined secret which will be used to encrypt the password and 
+// added the field which we actually want to encrypt
+// const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["Password"]
+});
 
 // CREATING MONGOOSE MODEL FOR AUTHENTICATION
 const User = mongoose.model("User", userSchema);
