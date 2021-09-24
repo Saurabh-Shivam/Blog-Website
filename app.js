8cc6160ac50b2409e1fd2c07e1e9f28eb71062cb
Saurabh <saurabhshivam4@gmail.com>
2021-09-24 13:36:28 +0530

    Hashing the password with md5

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
 
 const homeStartingContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum., written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,comes from a line in section 1.10.32.";
 const aboutPageContent = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of. The Extremes of Good and Evil) by Cicero";
@@ -59,13 +61,13 @@ const userSchema = new mongoose.Schema({
   Password: String
 });
 
-// Added encryption package to userSchema, defined secret which will be used to encrypt the password and 
-// added the field which we actually want to encrypt
-// const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["Password"]
-});
+// // Added encryption package to userSchema, defined secret which will be used to encrypt the password and 
+// // added the field which we actually want to encrypt
+// // const secret = "Thisisourlittlesecret.";
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ["Password"]
+// });
 
 // CREATING MONGOOSE MODEL FOR AUTHENTICATION
 const User = mongoose.model("User", userSchema);
@@ -120,7 +122,8 @@ app.post('/register', (req, res) => {
   const newUser = new User({
     Username: req.body.username,
     Email: req.body.email,
-    Password: req.body.password,
+    // Turning the password into a hash function using md5
+    Password: md5(req.body.password)
   });
 
   newUser.save(function (err) {
@@ -142,7 +145,7 @@ app.post('/login', (req, res) => {
 
   const lUsername = req.body.username;
   const lEmail = req.body.email;
-  const lPassword = req.body.password;
+  const lPassword = md5(req.body.password);
 
   User.findOne({
     Email: lEmail
